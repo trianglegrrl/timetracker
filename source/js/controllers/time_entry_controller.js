9cd1491b5f2a9c35b14e4a7f49f787c933dd6df8
minusfive <mail@minusfive.com>
2013-05-14 00:50:53 -0400

    Replaced isProject CSS hack with computed property on time_entry controller

--- source/js/controllers/time_entry_controller.js
+++ source/js/controllers/time_entry_controller.js
@@ -1,1 +1,9 @@
-ETT.TimeEntryController = Em.ObjectController.extend();
+ETT.TimeEntryController = Em.ObjectController.extend({
+  needs: ['project'],
+
+  // Detect whether the project controller is set
+  isProject: function() {
+    return this.get('controllers.project.name') != null;
+  }.property('controllers.project.name'),
+
+});
